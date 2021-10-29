78414762b8e1ce18574259ab880ac83211dd6ec7
Rodrigo Lessa <geekrodrigo@live.com>
2021-10-29 16:52:12 -0300

    First commit from VSCode

---
+++ script.js
@@ -0,0 +1,28 @@
+
+// new QRCode(document.getElementById("nanoQR"), "nano_1u55suaiojki8hj5kgtein4k5ndw17r45ty766puzjjxw6z7uw1atrzwz4fo");
+var matic = "0x3d71311D333594DBFD64d2f0D1A3b5Dd46569fC3";
+var bsc = "0x3d71311D333594DBFD64d2f0D1A3b5Dd46569fC3";
+var btc = "1BWiGQXapZYG9qYi7ZXQKYKa8RDbMgGVVN";
+var nano = "nano_1u55suaiojki8hj5kgtein4k5ndw17r45ty766puzjjxw6z7uw1atrzwz4fo";
+
+function shownano(){
+  $('.mini.modal.shownano')
+    .modal('show')
+  ;
+}
+function showbtc(){
+  $('.mini.modal.btcqr')
+    .modal('show')
+  ;
+}
+function showbsc(){
+  $('.mini.modal.bscqr')
+    .modal('show')
+  ;
+}
+function showPoly(){
+  $('.mini.modal.polyQR')
+    .modal('show')
+  ;
+}
+
