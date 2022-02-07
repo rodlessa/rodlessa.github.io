214654ae669a6e8b11dc08f1519425cabbf2abce
Rodrigo Lessa <rodlessa@users.noreply.github.com>
2022-02-07 08:25:46 -0300

    update wallets address

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
 // new QRCode(document.getElementById("nanoQR"), "nano_1u55suaiojki8hj5kgtein4k5ndw17r45ty766puzjjxw6z7uw1atrzwz4fo");
-var matic = "0x3d71311D333594DBFD64d2f0D1A3b5Dd46569fC3";
-var bsc = "0x3d71311D333594DBFD64d2f0D1A3b5Dd46569fC3";
+var matic = "0x01a99EE37Bae60cC554FcA8a9755C76917A40b14";
+var bsc = "0x01a99EE37Bae60cC554FcA8a9755C76917A40b14";
 var btc = "1BWiGQXapZYG9qYi7ZXQKYKa8RDbMgGVVN";
 var nano = "nano_1u55suaiojki8hj5kgtein4k5ndw17r45ty766puzjjxw6z7uw1atrzwz4fo";
+var harmony = "0x01a99EE37Bae60cC554FcA8a9755C76917A40b14";
 
 function shownano(){
   $('.mini.modal.shownano')
@@ -25,4 +26,8 @@ function showPoly(){
     .modal('show')
   ;
 }
-
+function showHarmony(){
+  $('.mini.modal.OneQR')
+    .modal('show')
+  ;
+}
