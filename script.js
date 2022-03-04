a74c000d810334ad64d2639062e6ef244462917c
Rodrigo Lessa <geekrodrigo@live.com>
2022-03-03 22:03:00 -0300

    Update index and weather.js

---
+++ script.js
@@ -0,0 +1,21 @@
+  $('#progresjs').progress({
+    percent: 56
+  });
+  $('#progreshtml').progress({
+    percent: 100
+  });
+  $('#progrescss').progress({
+    percent: 75
+  });
+  $('#progreslara').progress({
+    percent: 15
+  });
+  $('#progresnode').progress({
+    percent: 25
+  });
+  $('#progresec2').progress({
+    percent: 50
+  });
+  $('#progreslinux').progress({
+    percent: 60
+  });
--- weather.js
+++ weather.js
@@ -15,7 +15,7 @@ function weather( cityID ) {
     document.getElementById('min').innerHTML = "Temperatura minima: "+cmin+ '&deg;';
   })
   .catch(function() {
-    // catch any errors
+    // erros
   });
 }
 
@@ -26,12 +26,15 @@ window.onload = function() {
 
 function weathercity(cityname) {
   var cityname = document.getElementById('search_city').value;
-  var key = '452cb428ac2a43b958c8477f11f11050';
-  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityname+ '&appid=' + key)
-  .then(function(resp) { return resp.json() }) // Convert data to json
+  var key = 'df43061c90b4277aa5dd0bbd350d5bbc';
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityname + '&appid=' + key)
+  .then(function(resp) { return resp.json() })
   .then(function(data) {
     document.getElementById('city').innerHTML = data.name;
     document.getElementById('description').innerHTML = data.weather[0]['description'];
+    if (data.weather[0]['description'] === undefined ) {
+      alert("Possivel erro de chave de API, contate o admistrador!")
+    }
     icon = 'https://openweathermap.org/img/wn/'+data.weather[0]["icon"]+'@2x.png';
     var celcius = Math.round(parseFloat(data.main.temp)-273.15);
     var cmax = Math.round(parseFloat(data.main.temp_max)-273.15);
